67ceef83fe0256c609010f39fc7103525ae6462a
mayankdembla0407 <105827066+MayankDembla0407@users.noreply.github.com>
2024-04-23 19:01:16 -0500

    First Commit

---
+++ src/main/java/com/ucm/adbms/AdbmsApplication.java
@@ -0,0 +1,13 @@
+package com.ucm.adbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdbmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucm/adbms/AdbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucm.adbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
